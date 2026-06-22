ed9ca98d5d0b1193abadc1dd0a7e86ec1435cf28
Ben Moriarty <benmoriarty2004@gmail.com>
2026-06-22 07:11:10 -0700

    using correct property

--- netscan.ps1
+++ netscan.ps1
@@ -1,5 +1,5 @@
 $active_IPs = 1..254 | ForEach-Object -Parallel { Test-Connection -Count 1 192.168.0.$_ } -ThrottleLimit 10 |
 Where-Object Status -eq Success |
-Select-Object ComputerName, NetAdapter
+Select-Object Address
 
 Write-Host $active_IPs
